d8f25e55570919bce10e131ba740868e4af5d715
Ricardo Erikson <ricardoerikson@gmail.com>
2014-06-10 16:09:08 -0400

    Login fragment

--- src/br/ufmg/ppgee/secscrmob/MainActivity.java
+++ src/br/ufmg/ppgee/secscrmob/MainActivity.java
@@ -1,49 +1,49 @@
 package br.ufmg.ppgee.secscrmob;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+	if (savedInstanceState == null) {
+	    getFragmentManager().beginTransaction()
+		    .add(R.id.container, new PlaceholderFragment()).commit();
+	}
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.main, menu);
+	return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+	// Handle action bar item clicks here. The action bar will
+	// automatically handle clicks on the Home/Up button, so long
+	// as you specify a parent activity in AndroidManifest.xml.
+	int id = item.getItemId();
+	if (id == R.id.action_settings) {
+	    return true;
+	}
+	return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -51,15 +51,37 @@ public class MainActivity extends Activity {
      */
     public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+	EditText mUsernamField;
+	EditText mPasswordField;
+	Button mSubmitLoginButton;
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+	public PlaceholderFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+		Bundle savedInstanceState) {
+	    View rootView = inflater.inflate(R.layout.fragment_main, container,
+		    false);
+	    mUsernamField = (EditText) rootView
+		    .findViewById(R.id.username_field);
+	    mPasswordField = (EditText) rootView
+		    .findViewById(R.id.password_field);
+	    mSubmitLoginButton = (Button) rootView
+		    .findViewById(R.id.submit_login_button);
+	    mSubmitLoginButton.setOnClickListener(new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+		    Log.i("login", "Username:"
+			    + mUsernamField.getText().toString());
+		    Log.i("login", "Passord:"
+			    + mPasswordField.getText().toString());
+
+		}
+	    });
+
+	    return rootView;
+	}
     }
 
 }
